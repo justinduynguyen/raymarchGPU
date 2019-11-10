3de069d84bf9f0161cef400cc6df928ae812476c
duy nguyen <nguyentanduy1@gmail.com>
2019-11-11 02:47:00 +0700

    update raymarching PBR theory

--- GPUTest/Assets/GPUside/Raymarcher.cs
+++ GPUTest/Assets/GPUside/Raymarcher.cs
@@ -6,17 +6,29 @@ public class Raymarcher : MonoBehaviour
     public ComputeShader raymarching;
     public int threadGroupX, threadGroupY, threadGroupZ;
     RenderTexture result;
-    public Light dirLight;
+    public Texture enviroment;
     Camera _camera;
+    public Material antiAlias;
+    //public bool pause;
+    public Light dirLight;
+    public Texture skybox;
+     [Range(0,1)]
+    public float roughness;
+    RenderTexture _convered;
+    int idkernel;
+
     private void Awake()
     {
         _camera = GetComponent<Camera>();
+        _camera.depthTextureMode = DepthTextureMode.Depth;
+         idkernel = raymarching.FindKernel("CSMain");
     }
     // Start is called before the first frame update
     void CreateTexture()
     {
         if(result==null||result.width!=Screen.width||result.height!=Screen.height)
         {
+        
             if(result!=null)
             {
                 result.Release();
@@ -24,24 +36,37 @@ public class Raymarcher : MonoBehaviour
             result = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
             result.enableRandomWrite = true;
             result.Create();
+            _convered = new RenderTexture(Screen.width, Screen.height, 0,
+       RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            _convered.enableRandomWrite = true;
             threadGroupX = Screen.width / 8;
             threadGroupY = Screen.height / 8;
             threadGroupZ = 1;
         }
     }
-
-
+ 
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
         CreateTexture();
-        int idkernel = raymarching.FindKernel("CSMain");
+      
         raymarching.SetTexture(idkernel, "Result", result);
         raymarching.SetMatrix("cameraToWorld", _camera.cameraToWorldMatrix);
         raymarching.SetMatrix("cameraToProjectionInverse", _camera.projectionMatrix.inverse);
-        raymarching.SetVector("directionalLight", 
+        Texture depth = Shader.GetGlobalTexture("_CameraDepthTexture");
+        raymarching.SetTexture(idkernel, "_depthTexture", depth);
+        raymarching.SetVector("directionalLight",
             new Vector4(dirLight.transform.forward.x, dirLight.transform.forward.y, dirLight.transform.forward.z, dirLight.intensity));
+        raymarching.SetTexture(idkernel, "_SkyboxTexture", skybox);
+        raymarching.SetVector("lightColor",new Vector3(dirLight.color.r,dirLight.color.g,dirLight.color.b));
+        raymarching.SetFloat("roughness", roughness);
+        raymarching.SetFloat("_Time", Time.time);
+        raymarching.SetTexture(idkernel,"_rasTexture",source);
+      
         raymarching.Dispatch(idkernel, Mathf.CeilToInt(threadGroupX), Mathf.CeilToInt(threadGroupY), 1);
-        Graphics.Blit(result, destination);
+        Graphics.Blit(result, destination, antiAlias);     
+          //  Graphics.Blit(_convered, destination);
+           
+        }
     }
     
-}
+
---
+++ GPUTest/Assets/Shaders/ScreenEffect.shader
@@ -0,0 +1,54 @@
+﻿Shader "Hidden/ScreenEffect"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+	   _Src("Src",2D)="white"{}
+	    
+    }
+    SubShader
+    {
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
+	
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #define vec2 float2
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+	
+            sampler2D _MainTex;
+			sampler2D _Src;
+            float4 frag (v2f i) : SV_Target
+            {
+            float4 add = tex2D(_MainTex, i.uv);
+			float4 src = tex2D(_Src, i.uv);
+			float4 col = lerp(src, add, add.w);
+			return add;
+            }
+            ENDCG
+        }
+    }
+}
